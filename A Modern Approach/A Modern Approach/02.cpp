2400bbebe0e642d14ee6343e92b358d537f44871
裕聪 郑 <the-moments@live.cn>
2016-05-18 13:03:17 +0800

    0518

---
+++ A Modern Approach/A Modern Approach/02.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//int check(int, int, int);
+//
+//int main(void) {
+//	int x, y, n;
+//	scanf("%d %d %d", &x, &y, &n);
+//	check(x, y, n);
+//	return 0;
+//}
+//
+//int check(int x, int y, int n) {
+//	if (x >= 0 && x < n && y >= 0 && y < n)
+//		return 1;
+//	else
+//		return 0;
+//}
